8cd20c4ce45ba0433cb6c7d12930fce78076f405
RobertEverthon <112262678+RobertEverthon@users.noreply.github.com>
2024-06-05 08:51:15 -0300

    Update main.py
    Adição de descrição do main.

--- mercadinho-api/app/main.py
+++ mercadinho-api/app/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from app.models.product import Base
-from app.core.database import engine
-from app.routers import some_router
+from fastapi import FastAPI #Importamos O fastAPI
+from app.models.product import Base #Importamos do diretório "models" a classe "product."
+from app.core.database import engine #Importamos de "core" o "database"
+from app.routers import some_router #Importamos "some_router" do módulo "routers"
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine) # Cria as tabelas no banco de dados.
 
-app = FastAPI()
+app = FastAPI()  #Cria a instância da classe fastapi chamada de "app".
 
-app.include_router(some_router.router)
+app.include_router(some_router.router) #Coloca as rotas definidas para o "app".
