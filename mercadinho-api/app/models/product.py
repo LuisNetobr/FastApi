4ba284253a62f47dcff780e79338bcb26831288d
RobertEverthon <112262678+RobertEverthon@users.noreply.github.com>
2024-06-05 21:06:33 -0300

    Descrição product.py
    Adição de descrição do product.

--- mercadinho-api/app/models/product.py
+++ mercadinho-api/app/models/product.py
@@ -1,6 +1,18 @@
+"""O código abaixo é responsável por criar o nosso produto. """
+
+"""Abaixo importamos da biblioteca sqlalchemy algumas de suas funções
+para criar colunas e definir tipos de valores. """
 from sqlalchemy import Column, Integer, String, Float
+
+"""Importamos de database a classe Base"""
 from app.core.database import Base
 
+"""Aqui criamos a classe Product, que vai ser o nosso produto, que herda de Base
+(classe base para a criar tabelas) e dentro da classe, definimos o nome da tabela,
+coluna de id, nome, preço e quantidade do produto, juntamente com o tipo de valores
+que vão ter (Integer, String, Float). 
+A coluna id é a chave primaria, e o index=true cria um indice para a coluna "id" e "name"
+para que seja possível encontrar rapidamente os valores na tabela."""
 class Product(Base):
     __tablename__ = "products"
 
