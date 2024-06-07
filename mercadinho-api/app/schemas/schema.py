00082dc0f951e2fac1ece1cd32efdc7f3d007366
Bruno Marcel <109152699+brunoomarcel@users.noreply.github.com>
2024-06-07 13:19:29 -0300

    Update schema.py
    Neste arquivo, definimos o schema de dados para produtos utilizando o Pydantic.

--- mercadinho-api/app/schemas/schema.py
+++ mercadinho-api/app/schemas/schema.py
@@ -1,15 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel # Importa a classe BaseModel do Pydantic para a criação de esquemas de validação de dados
 
+#Essa é a classe base para os dados de um produto
 class ProductBase(BaseModel):
     name: str
     quantity: int
     price: float
 
-
+#Classe para criação de um produto, herdando de ProductBase
 class CreateProduct(ProductBase):
-    pass
+    pass # Não adiciona novos campos, apenas utiliza os campos de ProductBase
 
+#Classe para representar um produto com ID
 class Product(ProductBase):
     id: int
+
+    #Configurações adicionais da classe
     class Config:
-        orm_mode = True
+        orm_mode = True #Habilita o modo ORM para compatibilidade com modelos ORM do SQLAlchemy
